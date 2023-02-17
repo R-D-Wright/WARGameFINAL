e5e02dbce17c6302830139b000fd3a791baa78fc
Drake Wright <rdrakewright@gmail.com>
2023-02-17 08:20:17 -0600

    Initial Commit

---
+++ WARGame/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WARGame
+//
+//  Created by Drake Wright on 2/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("War Card Game")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WARGame/WARGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WARGameApp.swift
+//  WARGame
+//
+//  Created by Drake Wright on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WARGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
